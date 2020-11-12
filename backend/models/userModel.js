71cbfe6100fdd007c059df2e74dba63a257fd2e0
Fajar-islami <ahmadfajarislami@protonmail.com>
2020-11-12 07:48:41 +0700

    Video 37 || User authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
   {
@@ -23,6 +24,11 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true },
 );
+
+// Sebelum brcrypt di controller
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
