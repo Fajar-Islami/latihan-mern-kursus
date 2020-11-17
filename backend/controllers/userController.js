abc6938955d53aec86cec3bfee1618882604e1dc
Fajar-islami <ahmadfajarislami@protonmail.com>
2020-11-17 14:25:50 +0700

    Video 70 || Endpoint get user by id & update user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -130,6 +130,44 @@ const deleteUsers = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get user by ID
+// @route   GET /api/users/:od
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password'); // tidak ambil password
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @desc    Update user profile
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin || user.isAdmin;
+
+    const updateUser = await user.save();
+
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 export {
   authUser,
   registerUser,
@@ -137,4 +175,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUsers,
+  getUserById,
+  updateUser,
 };
